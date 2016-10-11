907b8350a48ef61d90ab1c750dab4ef216c93f6e
brownac <austincbrown@ufl.edu>
2016-10-10 21:45:39 -0400

    init

---
+++ js/landing-page.js
@@ -0,0 +1,4 @@
+$('#scene').parallax();
+$(document).ready(function () {
+    $('div.enter-link').fadeIn(2000).removeClass('.enter-link');
+})
---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+//typeit
+$('#typed').typeIt({
+    strings: ['University of Florida | January 20-22, 2017'],
+    speed: 100,
+    lifeLike: true, 
+    autoStart: true,
+});       
+
+// google analytics
+ (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-85409295-1', 'auto');  
+    ga('send', 'pageview');
