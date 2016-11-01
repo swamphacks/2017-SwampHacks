1733ca3265e8de05b6a782678b9be69a4235819e
brownac <austincbrown@ufl.edu>
2016-10-31 23:08:54 -0400

    added an email/password system

--- js/resume.js
+++ js/resume.js
@@ -13,10 +13,21 @@ $('#resume').on("change", function (event) {
    selectedFile = event.target.files[0];
 });
 
-$('#submit-resume').click(function() {
+$('#submit-info').click(e => {
     var fileName = selectedFile.name;
     console.log(fileName);
     var storageRef = firebase.storage().ref('/resumes/' + fileName);
     var uploadTask = storageRef.put(selectedFile);
     //TODO: add a progress bar
+
+    //put the email and password in the database
+    e.preventDefault();
+    var email = $('#email').val();
+    var password = $('#password').val();
+    firebase.database().ref('users').push({
+        email,
+        password
+    }).then(() => {
+        window.location.replace("index.html");
+    });
 });
