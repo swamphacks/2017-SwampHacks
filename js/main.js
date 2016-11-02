b8441c3dcb69bd2ccbacb27c1420f0e517ac20e4
brownac <austincbrown@ufl.edu>
2016-11-01 22:37:37 -0400

    added the reeds

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 $(document).ready(function () {
+
+    //$("#scene").parallax();
     //change the  background based on the time of day
     var date = new Date();
     var hrs = date.getHours();
     console.log(hrs);
     var title = $("#title");
-    if (hrs > 19 || hrs < 6) {
+    if (hrs >= 19 || hrs < 6) {
         title.addClass("night");
-    } else if(hrs > 16 && hrs < 19) {
+    } else if(hrs >= 16 && hrs < 19) {
         title.addClass("sunset");
-    } else if(hrs > 6 && hrs < 9) {
+    } else if(hrs >= 6 && hrs < 9) {
         title.addClass("sunrise");
     } else {
         title.addClass("day");
