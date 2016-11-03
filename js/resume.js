bbcad2bde4e688c3c60748dc64ae5cf3c3add880
brownac <austincbrown@ufl.edu>
2016-11-03 15:26:29 -0400

    Put the resume stuff before the application.

--- js/resume.js
+++ js/resume.js
@@ -14,6 +14,11 @@ $('#resume').on("change", function (event) {
 });
 
 $('#submit-info').click(e => {
+    if(selectedFile === null) {
+        //scold them for not uploading their resume. naughty children!
+        $('.error').text("Naughty child! Upload your resume.").show();
+    } else {
+
     var fileName = selectedFile.name;
     console.log(fileName);
     var storageRef = firebase.storage().ref('/resumes/' + fileName);
@@ -22,12 +27,13 @@ $('#submit-info').click(e => {
 
     //put the email and password in the database
     e.preventDefault();
-    var email = $('#email').val();
+    /**var email = $('#email').val();
     var password = $('#password').val();
     firebase.database().ref('users').push({
         email,
         password
-    }).then(() => {
-        window.location.replace("index.html");
-    });
+    })**/
+    window.location.replace("https://my.mlh.io/oauth/authorize?client_id=4152857ec50532295c5cc45ec8a48e35ec1f093c9370aaeb71dbc2b3efe8f4aa&redirect_uri=https%3A%2F%2Fbrownac.github.io%2Fthanks.html&response_type=token");
+
+}
 });
