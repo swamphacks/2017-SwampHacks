8fb3d3f5c0ae481dab0796398a719261ab68710b
brownac <austincbrown@ufl.edu>
2016-11-06 17:20:43 -0500

    Fields that have data get populated

--- js/resume.js
+++ js/resume.js
@@ -1,12 +1,71 @@
 var selectedFile;
-var config = {
-    apiKey: "AIzaSyCKrjgs7TMiPDl-nqaqnBTAhigNrNszQPk",
-    authDomain: "swamphacks-a6338.firebaseapp.com",
-    databaseURL: "https://swamphacks-a6338.firebaseio.com",
-    storageBucket: "swamphacks-a6338.appspot.com",
-    messagingSenderId: "949283718048"
+/**var app = firebase.initializeApp({
+    apiKey: firebase.api_key,
+    authDomain: firebase.auth_domain,
+    databaseURL: firebase.database_url,
+    storageBucket: firebase.storage_bucket
+});**/
+
+//get the data from mymlh; first, we need the access token.
+var access_token = window.location.href.split(/[&#]/)[1].replace('access_token=', '');
+var request_url = "https://my.mlh.io/api/v1/user/?access_token=" + access_token;
+console.log(request_url);
+
+function getData() {
+    window.location.replace('https://my.mlh.io/oauth/authorize?client_id=4152857ec50532295c5cc45ec8a48e35ec1f093c9370aaeb71dbc2b3efe8f4aa&redirect_uri=https%3A%2F%2Fbrownac.github.io%2Fresume.html&response_type=token');
+}
+
+$.ajax({
+    type: 'GET',
+    dataType: 'json',
+    url: request_url,
+    success: function(res) {
+        console.log(res.data);
+        var fields = $.map(res.data, function(val) {
+            return val;
+        });
+        console.log(fields);
+        populateForm(fields);
+    },
+    error: function(err) {
+        //whatever
+    }
+});
+        
+var populateForm = function(items) {
+    //get the values
+    var firstname = items[4];
+    var lastname = items[5];
+    var dob = items[10];
+    var gender = items[11];
+    var phone = items[12];
+    var major = items[6];
+    var email = items[1];
+    var school = items[13]['name'];
+    var diet = items[8];
+    var shirt = items[7];
+
+    //put them in the fields
+    $('#firstname').val(firstname);
+    $('#lastname').val(lastname);
+    $('#dob').val(dob);
+    $('#gender').val(gender);
+    $('#phone').val(phone);
+    $('#major').val(major);
+    $('#email').val(email);
+    //grad year?
+    $('#school').val(school);
+    $('#diet').val(diet);
+    $('#shirt').val(shirt)
+    //todo: see if there are any more fields we can get
+    
+    //submit to the database
+    firebase.database().ref('applicants').push({
+        firstname, lastname, dob, gender, phone, major, email,
+        school, diet, shirt
+    });
 };
-firebase.initializeApp(config);
+
 
 //watch for the uploaded files
 $('#resume').on("change", function (event) {
