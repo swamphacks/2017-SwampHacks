0e3757a8397d6cd82ff24919676834ce56048051
Nikhil Thota <nt92@ufl.edu>
2016-11-12 17:52:51 -0500

    redoing allllll the css's

--- js/animation.js
+++ js/animation.js
@@ -1,153 +1,153 @@
-$(function() {
-//$('#scene').animation();
-
-	//animating the waves
-	$(document).ready(function() {
-		//waves
-        setInterval(animateImageLeft, 1000);
-        setInterval(animateupImageLeft, 1000);
-        setInterval(animateImageRight, 1000);
-        setInterval(animateImageupRight, 1000);
-        floatThatStork();
-        rotateFish();
-        rotateOtherFish();
-        moveGator();
-        swayReeds();
-    });
-    var height = $(window).height();
-
-    function animateImageLeft() {
-        $("#wave1").animate({bottom: '-5%'}, 3000, 'swing', 1000);
-    }
-    function animateupImageLeft() {
-        $('#wave1').animate({bottom: '-1%'}, 3000, 'swing',1000 );
-    }
-    function animateImageRight() {
-        $("#wave2").animate({bottom: '-1%'}, 3000, 'swing', 1000);
-    }
-    function animateImageupRight() {
-        $('#wave2').animate({bottom: '-5%'}, 3000, 'swing', 1000);
-    }
-
-   //float that stork
-    var img = $("#stork"),
-        width = img.get(0).width,
-        screenWidth = $(window).width();
-
-    function floatThatStork() {
-        img.css({"fontSize": 110, "left": -width, "top":"0%"}).animate({
-						fontSize: 0
-						// "left": screenWidth,
-            // "top": "-200px"
-        }, {
-					duration: 2800,
-					easing: "swing",
-					step: function(t, fx){
-	        	var a = t / 57.296;
-	        	var x = 100 + Math.cos(a) * screenWidth;
-	        	var y = -200 + Math.sin(a) * 200;
-        		$(this).css({ left: x, top: y });
-    			},
-					complete: floatThatStork
-				}).delay(1500);
-    }
-
-    var gator = $("#gator"),
-        gatorWidth = gator.get(0).width,
-        //gatorScreenWidth = $(window).width(),
-        gatorDuration = 8000;
-    function moveGator() {
-        gator.css("left", -gatorWidth).animate({
-            "left": "50%"
-        }, gatorDuration, moveGator).delay(5000);
-    }
-    //rotate da fishes
-    var fish = $('#fishy');
-    function rotateFish() {
-        setTimeout(function () {
-            fish.rotate({
-                angle: -180,
-                center: ["50%", "100%"],
-                animateTo:0,
-                duration: 1500,
-                easing: $.easing.easeOutBack,
-                callback: function() {
-                    rotateFish1();
-                }
-            });
-        }, 500);
-    }
-
-		function rotateFish1() {
-        setTimeout(function () {
-            fish.rotate({
-                angle: 0,
-                center: ["50%", "100%"],
-                animateTo:180,
-                duration: 1500,
-                easing: $.easing.easeOutBack,
-                callback: function() {
-                    rotateFish();
-                }
-            });
-        }, 0);
-    }
-
-    var fish2 = $('#fishy2');
-    function rotateOtherFish() {
-        setTimeout(function () {
-            fish2.rotate({
-                angle: 180,
-                center: ["50%", "100%"],
-                animateTo: 0,
-                duration: 1500,
-                easing: $.easing.easeOutBack,
-                callback: function() {
-                    rotateOtherFish1();
-                }
-            });
-        }, 1000);
-    }
-
-		function rotateOtherFish1() {
-        setTimeout(function () {
-            fish2.rotate({
-                angle: 0,
-                center: ["50%", "100%"],
-                animateTo: -180,
-                duration: 1500,
-                easing: $.easing.easeOutBack,
-                callback: function() {
-                    rotateOtherFish();
-                }
-            });
-        }, 0);
-    }
-
-    var reeds = $('#reeds');
-    function swayReeds() {
-        reeds.rotate({
-            angle: 0,
-            center: ["50%", "100%"],
-           animateTo: -10,
-            duration: 2000,
-            easing: $.easing.easeOutBack,
-            callback: function () {
-                swayReeds1();
-            }
-        });
-    }
-    function swayReeds1() {
-        reeds.rotate({
-            angle: -10,
-            center: ["50%", "100%"],
-            animateTo: 0,
-            duration: 2000,
-            easing: $.easing.easeOutBack,
-            callback: function () {
-                swayReeds();
-            }
-        });
-    }
-
-});
+// $(function() {
+// //$('#scene').animation();
+//
+// 	//animating the waves
+// 	$(document).ready(function() {
+// 		//waves
+//         setInterval(animateImageLeft, 1000);
+//         setInterval(animateupImageLeft, 1000);
+//         setInterval(animateImageRight, 1000);
+//         setInterval(animateImageupRight, 1000);
+//         floatThatStork();
+//         rotateFish();
+//         rotateOtherFish();
+//         moveGator();
+//         swayReeds();
+//     });
+//     var height = $(window).height();
+//
+//     function animateImageLeft() {
+//         $("#wave1").animate({bottom: '-5%'}, 3000, 'swing', 1000);
+//     }
+//     function animateupImageLeft() {
+//         $('#wave1').animate({bottom: '-1%'}, 3000, 'swing',1000 );
+//     }
+//     function animateImageRight() {
+//         $("#wave2").animate({bottom: '-1%'}, 3000, 'swing', 1000);
+//     }
+//     function animateImageupRight() {
+//         $('#wave2').animate({bottom: '-5%'}, 3000, 'swing', 1000);
+//     }
+//
+//    //float that stork
+//     var img = $("#stork"),
+//         width = img.get(0).width,
+//         screenWidth = $(window).width();
+//
+//     function floatThatStork() {
+//         img.css({"fontSize": 110, "left": -width, "top":"0%"}).animate({
+// 						fontSize: 0
+// 						// "left": screenWidth,
+//             // "top": "-200px"
+//         }, {
+// 					duration: 2800,
+// 					easing: "swing",
+// 					step: function(t, fx){
+// 	        	var a = t / 57.296;
+// 	        	var x = 100 + Math.cos(a) * screenWidth;
+// 	        	var y = -200 + Math.sin(a) * 200;
+//         		$(this).css({ left: x, top: y });
+//     			},
+// 					complete: floatThatStork
+// 				}).delay(1500);
+//     }
+//
+//     var gator = $("#gator"),
+//         gatorWidth = gator.get(0).width,
+//         //gatorScreenWidth = $(window).width(),
+//         gatorDuration = 8000;
+//     function moveGator() {
+//         gator.css("left", -gatorWidth).animate({
+//             "left": "50%"
+//         }, gatorDuration, moveGator).delay(5000);
+//     }
+//     //rotate da fishes
+//     var fish = $('#fishy');
+//     function rotateFish() {
+//         setTimeout(function () {
+//             fish.rotate({
+//                 angle: -180,
+//                 center: ["50%", "100%"],
+//                 animateTo:0,
+//                 duration: 1500,
+//                 easing: $.easing.easeOutBack,
+//                 callback: function() {
+//                     rotateFish1();
+//                 }
+//             });
+//         }, 500);
+//     }
+//
+// 		function rotateFish1() {
+//         setTimeout(function () {
+//             fish.rotate({
+//                 angle: 0,
+//                 center: ["50%", "100%"],
+//                 animateTo:180,
+//                 duration: 1500,
+//                 easing: $.easing.easeOutBack,
+//                 callback: function() {
+//                     rotateFish();
+//                 }
+//             });
+//         }, 0);
+//     }
+//
+//     var fish2 = $('#fishy2');
+//     function rotateOtherFish() {
+//         setTimeout(function () {
+//             fish2.rotate({
+//                 angle: 180,
+//                 center: ["50%", "100%"],
+//                 animateTo: 0,
+//                 duration: 1500,
+//                 easing: $.easing.easeOutBack,
+//                 callback: function() {
+//                     rotateOtherFish1();
+//                 }
+//             });
+//         }, 1000);
+//     }
+//
+// 		function rotateOtherFish1() {
+//         setTimeout(function () {
+//             fish2.rotate({
+//                 angle: 0,
+//                 center: ["50%", "100%"],
+//                 animateTo: -180,
+//                 duration: 1500,
+//                 easing: $.easing.easeOutBack,
+//                 callback: function() {
+//                     rotateOtherFish();
+//                 }
+//             });
+//         }, 0);
+//     }
+//
+//     var reeds = $('#reeds');
+//     function swayReeds() {
+//         reeds.rotate({
+//             angle: 0,
+//             center: ["50%", "100%"],
+//            animateTo: -10,
+//             duration: 2000,
+//             easing: $.easing.easeOutBack,
+//             callback: function () {
+//                 swayReeds1();
+//             }
+//         });
+//     }
+//     function swayReeds1() {
+//         reeds.rotate({
+//             angle: -10,
+//             center: ["50%", "100%"],
+//             animateTo: 0,
+//             duration: 2000,
+//             easing: $.easing.easeOutBack,
+//             callback: function () {
+//                 swayReeds();
+//             }
+//         });
+//     }
+//
+// });
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    //$("#scene").parallax();
+    $("#scene").parallax();
     //change the  background based on the time of day
     var date = new Date();
     var hrs = date.getHours();
@@ -59,15 +59,14 @@ $(function(){
 /**$('#typed').typeIt({
     strings: ['University of Florida | January 20-22, 2017'],
     speed: 100,
-    lifeLike: true, 
+    lifeLike: true,
     autoStart: true,
-});**/       
+});**/
 
 // google analytics
  /**(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-    ga('create', 'UA-85409295-1', 'auto');  
+    ga('create', 'UA-85409295-1', 'auto');
     ga('send', 'pageview');**/
-
