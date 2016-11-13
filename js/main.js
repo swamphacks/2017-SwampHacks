356f97d5d1f987d224a8457f7baf504efda35614
iTwicKz <twickes32@ufl.edu>
2016-11-12 19:39:17 -0500

    Header animations

--- js/main.js
+++ js/main.js
@@ -21,6 +21,7 @@ $(document).ready(function () {
     $('h4.date').fadeIn(3000).removeClass('.date');
 });
 
+
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -41,33 +42,71 @@ $(function(){
   $("body").removeClass("preload");
   // Show/hide the menu bar based on scrolling position
   var vh = $(window).height() - 200;
-  $(".navbar").hide();
+  // $(".navbar").hide();
   $(function () {
     $(window).scroll(function () {
       if ($(this).scrollTop() > vh) {
-        $('.navbar').fadeIn();
+        // $('.navbar').fadeIn();
+        $('nav').removeClass('nav-top');
       } else {
-        $('.navbar').fadeOut();
+        // $('.navbar').fadeOut();
+        $('nav').addClass('nav-top');
       }
     });
   });
 });
 
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('nav').outerHeight();
 
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+
+    var st = $(this).scrollTop();
+
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('nav').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('nav').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+
+    lastScrollTop = st;
+}
 
 //typeit
 /**$('#typed').typeIt({
     strings: ['University of Florida | January 20-22, 2017'],
     speed: 100,
-    lifeLike: true, 
+    lifeLike: true,
     autoStart: true,
-});**/       
+});**/
 
 // google analytics
  /**(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-    ga('create', 'UA-85409295-1', 'auto');  
+    ga('create', 'UA-85409295-1', 'auto');
     ga('send', 'pageview');**/
-
