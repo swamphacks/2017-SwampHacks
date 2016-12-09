b7cecec344f39d3b1e92fe290702720a3b06ce2e
brownac <austincbrown@ufl.edu>
2016-12-09 13:45:19 -0500

    more team page additions

--- js/gifs.js
+++ js/gifs.js
@@ -10,4 +10,14 @@ $(function() {
       $(this).attr("src", "img/gifs/stills/group.jpg");
     }
   );
+
+  //austin
+  $(".austin").hover(
+    function() {
+      $(this).attr("src", "img/gifs/austin.gif");
+    },
+    function() {
+      $(this).attr("src", "img/gifs/stills/austin.jpg");
+    }
+  );
 });
