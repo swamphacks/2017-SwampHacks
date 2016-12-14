fb3ae032d2e5ade834ec2602c47240c1f1a8972c
brownac <austincbrown@ufl.edu>
2016-12-14 11:43:59 -0500

    added all the gifs. still a wip

--- js/gifs.js
+++ js/gifs.js
@@ -20,4 +20,60 @@ $(function() {
       $(this).attr("src", "img/gifs/stills/austin.jpg");
     }
   );
+  $(".alexa").hover(
+    function() {
+      $(this).attr("src", "img/gifs/alexa.gif");
+    },
+    function() {
+      $(this).attr("src", "img/gifs/stills/alexa.jpg");
+    }
+  );
+  $(".andrew").hover(
+    function() {
+      $(this).attr("src", "img/gifs/andrew.gif");
+    },
+    function() {
+      $(this).attr("src", "img/gifs/stills/andrew.jpg");
+    }
+  );
+  $(".caleb").hover(
+    function() {
+      $(this).attr("src", "img/gifs/caleb.gif");
+    },
+    function() {
+      $(this).attr("src", "img/gifs/stills/caleb.jpg");
+    }
+  );
+  $(".gianne").hover(
+    function() {
+      $(this).attr("src", "img/gifs/gianne.gif");
+    },
+    function() {
+      $(this).attr("src", "img/gifs/stills/gianne.jpg");
+    }
+  );
+  $(".Gonzi").hover(
+    function() {
+      $(this).attr("src", "img/gifs/Gonzi.gif");
+    },
+    function() {
+      $(this).attr("src", "img/gifs/stills/Gonzi.jpg");
+    }
+  );
+  $(".javier").hover(
+    function() {
+      $(this).attr("src", "img/gifs/javier.gif");
+    },
+    function() {
+      $(this).attr("src", "img/gifs/stills/javier.jpg");
+    }
+  );
+  $(".jeff").hover(
+    function() {
+      $(this).attr("src", "img/gifs/jeff.gif");
+    },
+    function() {
+      $(this).attr("src", "img/gifs/stills/jeff.jpg");
+    }
+  );
 });
