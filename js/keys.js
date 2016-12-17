5e4858ef75a11db44aeb5d2f4e824a40d97fdc29
brownac <austincbrown@ufl.edu>
2016-12-17 17:15:59 -0500

    removed keys from gitignoreo

---
+++ js/keys.js
@@ -0,0 +1,15 @@
+var keys = {
+  apiKey: "AIzaSyCKrjgs7TMiPDl-nqaqnBTAhigNrNszQPk",
+  authDomain: "swamphacks-a6338.firebaseapp.com",
+  databaseURL: "https://swamphacks-a6338.firebaseio.com",
+  storageBucket: "swamphacks-a6338.appspot.com",
+  messagingSenderId: "949283718048"
+};
+
+var confirmed_keys = {
+  apiKey: "AIzaSyDz1rvxuYDgsqMrGMdZ4iBIOqUBGETAo04",
+  authDomain: "swamphacks-confirmed-attendees.firebaseapp.com",
+  databaseURL: "https://swamphacks-confirmed-attendees.firebaseio.com",
+  storageBucket: "swamphacks-confirmed-attendees.appspot.com",
+  messagingSenderId: "1008919618491"
+};
