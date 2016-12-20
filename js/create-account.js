bdbbbc2fdde4cfdf74f290f07344f7cd9f3fda3f
brownac <austincbrown@ufl.edu>
2016-12-20 15:19:15 -0500

    Changed ref

--- js/create-account.js
+++ js/create-account.js
@@ -41,7 +41,7 @@ var submitData = function(email, pass, name) {
     const fileName = selected.name;
     const storageRef = firebase.storage().ref('/resumes/' + fileName);
     const uploadTask = storageRef.put(selectedFile);
-  firebase.database().ref('confirmed-attendees').child(email.toString().replace(".", "").replace("@", "")).set({
+  firebase.database().ref('confirmed').child(email.toString().replace(".", "").replace("@", "")).set({
     email, name
   })
   .then(() => {
